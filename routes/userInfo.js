211be80cd0afb64a2a0b61c012b7c9706c3803d0
Kha Leo <lexuankha2409@gmail.com>
2019-10-26 14:47:49 +0700

    add profile page

--- routes/userInfo.js
+++ routes/userInfo.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('userInfo/userInfo', {title: "user info", user: req.user});
 });
 
 module.exports = router;
