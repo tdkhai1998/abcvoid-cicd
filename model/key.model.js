23376397ba781e3017c5e60fbd7a585895cea8c8
unknown <huycong2798@gmail.com>
2019-10-26 14:45:27 +0700

    update: add route get,post payment

--- model/key.model.js
+++ model/key.model.js
@@ -7,6 +7,9 @@ module.exports = {
   singleById: id => {
     return db.load(`select * from api_key where id='${id}'`);
   },
+  update: (tableName, idField, entity)=>{
+    db.update(tableName, idField, entity);
+  },
   createEntity:()=>({
     user: "",
     key:""
