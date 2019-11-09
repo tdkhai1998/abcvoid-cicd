625d06a77a06dbf877e48a84a0b0761a2c408f10
wedday2004 <48178473+wedday2004@users.noreply.github.com>
2019-11-09 23:15:38 +0700

    Revert "fix routes post /packages/verify"
    This reverts commit 13581b317653a91a111165fd57379cdc89f1948b.

--- routes/packages.js
+++ routes/packages.js
@@ -83,47 +83,44 @@ router.get("/buy/:id", async (req, res, next) => {
 router.get("/verify", function(req, res, next) {
   console.log(req.user);
   const user = req.user;
-  if (!user) {
-    return res.redirect("/login");
+  if(!user)
+  {
+    return res.redirect("/login");  
   }
-
-  res.render("checkcode/checkcode", {
-    title: "Verify OTP",
-    user,
-    message
+  
+  return res.render("checkcode/checkcode",{
+    title:"Verify OTP",
+    user
   });
-  message = false;
+  
+  
 });
 router.post("/verify", async (req, res, next) => {
   console.log(req.user);
   const user = req.user;
-  if (!user) {
-    return res.redirect("/login");
+  if(!user)
+  {
+    return res.redirect("/login");  
   }
   const code = req.body.code;
-  console.log("code---", code);
   const api_key = await toFunc(keyModel.getKeyByTransactionId(code));
-  if (api_key[0]) {
+  if(api_key[0])
+  {
     next(api_key[0]);
-  } else {
-    if (api_key[1].length === 0) {
-      message = "OTP code is invalid please try again :( !";
-      console.log("messs---",message);
-      
-    } else {
-      console.log("valid---", api_key[1][0]);
-      api_key[1][0].valid = true;
-
-      // const result = await toFunc(keyModel.update(api_key[1][0]));
-      // if (result[0]) {
-      //   next(result[0]);
-      // } else {
-        keyModel.update(api_key[1][0])
-        message = "Your key is activated successfully !!!"
-        //res.redirect("/");
-      //}
-    }
-    res.redirect("/packages/verify");
   }
+  else {
+    api_key[1][0].valid = true;
+    console.log(api_key[1][0]);
+    const result = await toFunc(keyModel.update(api_key[1][0]));
+    if(result[0])
+    {
+      next(result[0]);
+    }
+    else {
+      res.redirect("/profile");
+    }
+  }
+  
+  
 });
 module.exports = router;
--- routes/user/userInfo.js
+++ routes/user/userInfo.js
@@ -1,22 +1,43 @@
 var express = require("express");
 var router = express.Router();
 const userModel = require("../../model/user.model");
+const apiKeyModel = require("../../model/key.model");
+const packageModel = require("../../model/packageKey.model");
 const toFunction = require("../../util/toFunction");
-let message = false;
+const genKey = require("../../function/genarateKey");
+const moment = require('moment');
 /* GET users listing. */
 router.get("/", async (req, res, next) => {
   const user = req.user;
   console.log(user);
   const infoUser = await toFunction(userModel.findApiKeys(user.id));
-  if (infoUser[0]) {
+  const listKey = await toFunction(apiKeyModel.getKeyById(user.id));
+  listKey[1] = listKey[1].map(elem => {
+    elem.date_expired =  moment(elem.date_expired).format('DD/MM/YYYY');
+    return elem;
+  });
+  if (infoUser[0] || listKey[0]) {
     return next(infoUser[0]);
   }
+  console.log('listKey----', listKey[1]);
   res.render("userInfo/userInfo", {
     user,
-    packages: infoUser[1],
-    message
+    listKey: listKey[1],
+    packages: infoUser[1]
   });
-  message = false;
+});
+router.post("/updatekey", async (req,res,next) => {
+  let key = await apiKeyModel.singleById(req.body.id);
+  key[0].value = genKey();
+  await apiKeyModel.update('id', key[0]);
+  res.redirect("/profile");
+});
+router.post("/renewkey", async (req, res, next) => {
+  let key = await apiKeyModel.singleById(req.body.idKey);
+  let packageInfo = await packageModel.singleById(req.body.id_package);
+  key[0].date_expired = moment(key[0].date_expired).add(packageInfo[0].term, 'days').format('YYYY-MM-DD');
+  await apiKeyModel.update('id',key[0]);
+  res.redirect("/profile");
 });
 router.post("/", async (req, res, next) => {
   const user = req.user;
