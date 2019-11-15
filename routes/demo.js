c631f3b98f4ba3f818d3fbe1290627df62c0a211
unknown <huycong2798@gmail.com>
2019-11-15 21:58:39 +0700

    them dinh dang ho tro la wav,wave va x-wav

--- routes/demo.js
+++ routes/demo.js
@@ -10,7 +10,7 @@ var storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
   console.log(file);
-  if (file.mimetype === "audio/wav") {
+  if (file.mimetype === "audio/wav" || file.mimetype === "audio/wave" || file.mimetype === "audio/x-wav") {
     cb(null, true);
   } else {
     cb(new Error("Chỉ chấp nhận file .wav"), false);
@@ -18,7 +18,7 @@ const fileFilter = (req, file, cb) => {
 };
 var upload = multer({
   storage: storage,
-  //fileFilter: fileFilter
+  fileFilter: fileFilter
 });
 
 router.get("/", transcribe_middleware.index);
