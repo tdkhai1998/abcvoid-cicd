a468091a6c415d60773cf4b23e4807bb3824c92e
tdkhai1998 <trandinhkhai10.1@gmail.com>
2019-11-16 02:43:17 +0700

    test

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
+  // re
   res.status(err.status || 500);
   if (err.status === 404) {
     res.render("error/404");
