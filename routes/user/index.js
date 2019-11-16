e142a7a491d03cd57fbfc2c69339a2dbf82ce101
tdkhai1998 <42509231+tdkhai1998@users.noreply.github.com>
2019-11-16 12:26:10 +0700

    add es6 eslint
    add es6 eslint

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,9 +1,7 @@
-module.exports = {
-  logout: require("./logout"),
-  recoverPassword: require("./recoverPassword"),
-  profile: require("./userInfo"),
-  login: require("./login"),
-  changePass: require("./recoverPassword"),
-  forgotPass: require("./passwordForgot"),
-  register: require("./register")
-};
+export { default as logout } from "./logout";
+export { default as recoverPassword } from "./recoverPassword";
+export { default as profile } from "./userInfo";
+export { default as login } from "./login";
+export { default as changePass } from "./recoverPassword";
+export { default as forgotPass } from "./passwordForgot";
+export { default as register } from "./register";
