1552e99b6a4275a292a08ab9e1afb26a84ca78ec
tdkhai1998 <42509231+tdkhai1998@users.noreply.github.com>
2019-11-17 18:04:19 +0700

    Fix bug wrong file type (#10)
    * test
    
    * test
    
    * add eslint
    
    * a
    
    * add es6 eslint
    
    * test eslint
    
    * a
    
    * f
    
    * f
    
    * fix-bug-sign-up
    
    * fix bug wrong file type

--- routes/demo/index.js
+++ routes/demo/index.js
@@ -2,31 +2,16 @@ var express = require("express");
 var router = express.Router();
 var transcribe_middleware = require("../../middleware/transcribe");
 var request = require("request");
+
+var uploadSingleFile = require("./upload");
 // packages upload file
 const multer = require("multer");
-
-// SET STORAGE
-var storage = multer.memoryStorage();
-
-const fileFilter = (req, file, cb) => {
-  console.log(file);
-  // if (file.mimetype === "audio/wav" || file.mimetype === "audio/wave" || file.mimetype === "audio/x-wav") {
-  cb(null, true);
-  // } else {
-  //   cb(new Error("Chỉ chấp nhận file .wav"), false);
-  // }
-};
 var upload = multer({
-  storage: storage,
-  fileFilter: fileFilter
+  storage: multer.memoryStorage(),
+  fileFilter: (req, file, cb) => {
+    cb(null, true);
+  }
 });
-
-router.get("/", transcribe_middleware.index);
-
-router.post(
-  "/",
-  upload.single("myFile"),
-  transcribe_middleware.uploadSingleFile
-);
-
+router.get("/", uploadSingleFile.get);
+router.post("/", upload.single("myFile"), uploadSingleFile.post);
 module.exports = router;
---
+++ routes/demo/upload.js
@@ -0,0 +1,66 @@
+var request = require("request");
+
+module.exports = {
+  post: async (req, res, next) => {
+    let wdRes = res;
+    let wdReq = req;
+    const url = "https://server-sound-api.herokuapp.com";
+    let jsonData = "";
+    const file = req.file;
+    if (
+      file.mimetype !== "audio/wav" &&
+      file.mimetype !== "audio/wave" &&
+      file.mimetype !== "audio/x-wav"
+    ) {
+      wdReq.session.message = "Chỉ chấp nhận file .wav";
+      wdRes.redirect("/demo");
+      return;
+    }
+    if (!file) {
+      wdReq.session.message = "Please upload a file";
+      wdRes.redirect("/demo");
+      return;
+    }
+    var formData = {
+      voice: {
+        value: file.buffer,
+        options: {
+          filename: file.originalname,
+          contentType: file.mimetype
+        }
+      }
+    };
+    request.post({ url: url, formData: formData }, async (err, res, body) => {
+      if (err) {
+        wdReq.session.error = error;
+        wdRes.redirect("/demo");
+        return;
+      } else {
+        jsonData = JSON.parse(body);
+        console.log("Upload successful!  Server responded with:", jsonData);
+        wdReq.session.filename = file.originalname;
+        wdReq.session.jsonData = jsonData["text"];
+        wdRes.redirect("/demo");
+      }
+    });
+  },
+  get: async (req, res, next) => {
+    let jsonData = "";
+    let descriptionFile = "";
+    if (req.session.filename) {
+      jsonData = req.session.jsonData;
+      descriptionFile = req.session.filename;
+    }
+    let message = req.session.message;
+    console.log(message);
+    req.session.message = null;
+    req.session = null;
+    res.render("./demo/demo", {
+      title: "Phiên dịch",
+      jsonData: jsonData,
+      descriptionFile: descriptionFile,
+      user: req.user,
+      message: message
+    });
+  }
+};
