ef3afa38817f0d85647067f9d3d27f1c45695f10
tdkhai1998 <trandinhkhai10.1@gmail.com>
2019-11-23 09:15:43 +0700

    fix eslint

--- middleware/transcribe.js
+++ middleware/transcribe.js
@@ -1,5 +1,4 @@
 var request = require("request");
-var keyModel = require("../model/key.model");
 exports.uploadSingleFile = async (req, res, next) => {
   console.log("dcfvgbhnnhhhhhhhhhhhh");
   let wdRes = res;
@@ -8,8 +7,12 @@ exports.uploadSingleFile = async (req, res, next) => {
   const url = "https://server-sound-api.herokuapp.com";
   let jsonData = "";
   const file = req.file;
-  console.log('file in request---', file);
-  if (file.mimetype !== "audio/wav" && file.mimetype !== "audio/wave" && file.mimetype !== "audio/x-wav") {
+  console.log("file in request---", file);
+  if (
+    file.mimetype !== "audio/wav" &&
+    file.mimetype !== "audio/wave" &&
+    file.mimetype !== "audio/x-wav"
+  ) {
     const error = new Error("Chỉ chấp nhận file .wav");
     error.httpStatusCode = 400;
     return next(error);
@@ -43,7 +46,7 @@ exports.uploadSingleFile = async (req, res, next) => {
   });
 };
 
-exports.index = async (req, res, next) => {
+exports.index = async (req, res) => {
   let jsonData = "";
   let descriptionFile = "";
   if (req.session.filename) {
